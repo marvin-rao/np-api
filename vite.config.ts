c04f68ca7d7fd00da9c417aab3f4b431c5674678
Blessing <blessing@4i.co.za>
2024-11-08 07:13:39 +0200

    _

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// @ts-ignore
 import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
+            // @ts-ignore
             '@': path.resolve(__dirname, './src'),
+            // @ts-ignore
             '@np': path.resolve(__dirname, './np')
         }
     },
