9926e22a9d857e40078074cc9b9ee4992a28be7c
Blessing <blessing@4i.co.za>
2024-11-10 08:03:02 +0200

    _

--- np/components/NewPaperProvider.tsx
+++ np/components/NewPaperProvider.tsx
@@ -15,16 +15,14 @@ export const NewPaperProvider = (props: AuthProviderProps) => {
   const { projectId } = useProjectId();
   const { shouldLogin } = useAuthSession();
   const [isOpen, setIsOpen] = useState(true);
-  console.log("projectId", projectId);
 
   return (
     <AuthProvider {...props}>
-      {!projectId && (
+      {!shouldLogin && !projectId && (
         <WorkspaceSelector
           open={isOpen}
           demoMode={false}
           onSelect={(workspace) => {
-            // setQueryParam("projectId", workspace.id);
             openWorkspace({ id: workspace.id });
             setIsOpen(false);
           }}
