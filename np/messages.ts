968f7d2e99030d6454bb77572b45a9dcd026dcea
Blessing <blessing@4i.co.za>
2025-07-05 16:07:37 +0200

    _

---
+++ np/messages.ts
@@ -0,0 +1,7 @@
+import { useProjectGetBase } from "./projects";
+
+export const useChatsUnreadCount = () => {
+    return useProjectGetBase<{ count: number }>({
+        path: "/chat/chats/unread_count",
+    });
+};
