a18c5f677e79c7daa39d4522a2f4d978abda5523
marvin-rao <30743056+marvin-rao@users.noreply.github.com>
2026-05-19 14:51:34 +0200

    _

--- np/npai-workspace.tsx
+++ np/npai-workspace.tsx
@@ -51,6 +51,8 @@ export interface NpAiWorkspaceChatMessage {
     input_tokens: number;
     output_tokens: number;
     total_tokens: number;
+    cache_creation_input_tokens?: number;
+    cache_read_input_tokens?: number;
   };
 }
 
